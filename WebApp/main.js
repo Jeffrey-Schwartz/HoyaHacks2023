a3038e7f79811b8147f3d846b5b7bfb24b46ba43
Jeffrey-Schwartz <schwartz@physics.ucla.edu>
2023-01-29 12:04:05 -0500

    Remove nested git

---
+++ WebApp/main.js
@@ -0,0 +1,20 @@
+
+
+
+  const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+  const buttons = document.querySelectorAll("button");
+  
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      audio.play();
+    });
+  });
+
+  Window.alert("Got your helmet? ")
+
+const get= document.getElementById("video");
+get.play();
+
+function 
+    
+}
